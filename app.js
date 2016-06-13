fce085e94203318ecccba87baf180b4e0e9604dc
Adam Braus <ajbraus@gmail.com>
2016-06-12 18:08:40 -0600

    adding favicon

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var retreats = require('./routes/retreats');
